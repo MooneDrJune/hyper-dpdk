50f7e04e3d6defb120a8a0e057602ea263cdc2a7
Quincy <qiujie.sqj@gmail.com>
2024-06-26 14:34:09 +0800

    modify: Change the tokio dependency to the private repository tokio, and change the client in the examples to dpdk client

--- examples/client.rs
+++ examples/client.rs
@@ -1,81 +1,76 @@
-#![deny(warnings)]
-#![warn(rust_2018_idioms)]
 use std::env;
-
-use bytes::Bytes;
-use http_body_util::{BodyExt, Empty};
-use hyper::Request;
-use tokio::io::{self, AsyncWriteExt as _};
+use std::io::Read;
+use bytes::{Buf, Bytes};
+use http::Request;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite};
 use tokio::net::TcpStream;
+use http_body_util::{BodyExt, Empty};
 
 #[path = "../benches/support/mod.rs"]
 mod support;
 use support::TokioIo;
 
-// A simple type alias so as to DRY.
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() {
     pretty_env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    tokio::fstack_init(args.len(), args);
 
-    // Some simple CLI args requirements...
-    let url = match env::args().nth(1) {
-        Some(url) => url,
-        None => {
-            println!("Usage: client <url>");
-            return Ok(());
-        }
-    };
+    let url = String::from("https://api.binance.com/api/v3/depth?symbol=BNBBTC&limit=1000");
 
-    // HTTPS requires picking a TLS implementation, so give a better
-    // warning if the user tries to request an 'https' URL.
+
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
     let url = url.parse::<hyper::Uri>().unwrap();
-    if url.scheme_str() != Some("http") {
-        println!("This example only works with 'http' URLs.");
-        return Ok(());
-    }
 
-    fetch_url(url).await
-}
+    rt.block_on(async {
+        let local = tokio::task::LocalSet::new();
+        local.run_until(async move {
+            let host = url.host().expect("uri has no host");
+            let port = url.port_u16().unwrap_or(443);
+            let addr = format!("{}:{}", host, port);
+            println!("addr: {}", addr);
 
-async fn fetch_url(url: hyper::Uri) -> Result<()> {
-    let host = url.host().expect("uri has no host");
-    let port = url.port_u16().unwrap_or(80);
-    let addr = format!("{}:{}", host, port);
-    let stream = TcpStream::connect(addr).await?;
-    let io = TokioIo::new(stream);
+            let stream = TcpStream::connect(addr).await.expect("connect failed");
+            let io = TokioIo::new(stream);
 
-    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
-    tokio::task::spawn(async move {
-        if let Err(err) = conn.await {
-            println!("Connection failed: {:?}", err);
-        }
+            match hyper::client::conn::http1::handshake(io).await {
+                Ok((mut client, connection)) => {
+                    println!("Handshake successful!");
+
+                    tokio::task::spawn_local(async move {
+                        if let Err(e) = connection.await {
+                            eprintln!("Connection error: {}", e);
+                        }
+                    });
+
+                    let req = Request::builder()
+                        .method("GET")
+                        .uri(url)
+                        .body(Empty::<Bytes>::new())
+                        .unwrap();
+
+                    match client.send_request(req).await {
+                        Ok(mut response) => {
+                            println!("Response: {}", response.status());
+                            let mut reader = response.collect().await.unwrap().aggregate().reader();
+                            let mut body_str = String::new();
+                            reader.read_to_string(&mut body_str).unwrap();
+
+                            println!("Body: {}", body_str);
+                        }
+                        Err(e) => {
+                            eprintln!("Request error: {}", e);
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("handshake failed: {}", e);
+                }
+            }
+        }).await;
     });
-
-    let authority = url.authority().unwrap().clone();
-
-    let path = url.path();
-    let req = Request::builder()
-        .uri(path)
-        .header(hyper::header::HOST, authority.as_str())
-        .body(Empty::<Bytes>::new())?;
-
-    let mut res = sender.send_request(req).await?;
-
-    println!("Response: {}", res.status());
-    println!("Headers: {:#?}\n", res.headers());
-
-    // Stream the body, writing each chunk to stdout as we get it
-    // (instead of buffering and printing at the end).
-    while let Some(next) = res.frame().await {
-        let frame = next?;
-        if let Some(chunk) = frame.data_ref() {
-            io::stdout().write_all(&chunk).await?;
-        }
-    }
-
-    println!("\n\nDone!");
-
-    Ok(())
 }
+
